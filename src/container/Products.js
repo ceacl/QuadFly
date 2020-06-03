0633d274cb8288c04068cfa65b984e03c150a6fd
Jose Francisco Arias <jose.ar1996@live.com>
2020-06-03 12:12:14 -0600

    Update Products.js
    Products

--- src/container/Products.js
+++ src/container/Products.js
@@ -1,12 +1,42 @@
 import React, { Component } from 'react'
+import mavic2 from '../assets/Image/Home/banner.jpg';
 
-class Products extends Component
-{
-    render() 
-    {
+class Products extends Component {
+    render() {
         return (
             <div>
-                Cambiar2
+                <div className="container">
+
+                    <div className="row">
+
+                        <div className="col s12 m12 l12">
+
+                            <div className="card ">
+                                <div className="card-image waves-effect">
+                                    <img src={mavic2} alt="" />
+                                    <span className="card-title">Mavic Air 2</span>
+                                </div>
+
+                                <div className="card-content">
+                                    <span className="card-title activator black-text">Mavic Air 2</span>
+                                    <p className="black-text">Capta grandiosas escenas con el mejor drone hasta la fecha</p>
+
+
+
+                                    <a className="waves-effect waves-light btn black white-text" id="btn" >COMPRAR</a>
+                                    <a className="waves-effect waves-light btn black white-text" id="btn" >VER MÁS</a>
+                                    <a className="waves-effect waves-light btn black white-text" id="btn"><i class="material-icons">add_shopping_cart</i></a>
+
+
+                                </div>
+
+
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
             </div>
         )
     }
