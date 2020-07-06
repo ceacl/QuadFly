db254d0da4ec50b9c1f50d794754f087ee71dd1a
César Castro <cesar.castro@ucrso.info>
2020-07-05 19:06:58 -0500

    Cambios en footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,25 +11,22 @@ class Footer extends Component
               <div className="container">
                 <div className="row">
                   
-                  <div className="col l12 s12">
+                  <div className="col l12 s12 center align">
                     <h5 className="white-text">QUADFLY</h5>
                   </div>
 
-                  <div className="col s3">
+                  <div className="col s4 center align">
                     <li><Link to="/about-us" className="grey-text text-lighten-3" >Contacto</Link></li>
                   </div>
-                  <div className="col s3">
+                  <div className="col s4 center align">
                     <li><Link to="privacy" className="grey-text text-lighten-3">Privacidad</Link></li>
                   </div>
-                  <div className="col s3">
-                    <li><a className="grey-text text-lighten-3" href="https://www.facebook.com/">Comunidad</a></li>
-                  </div>
-                  <div className="col s3">
+                  <div className="col s4 center align">
                     <li><Link to="/workteam" className="grey-text text-lighten-3">Equipo de trabajo</Link></li>
                   </div>
                 </div>
               </div>
-              <div className="footer-copyright">
+              <div className="footer-copyright center-align">
                 <div className="container">
                 © 2020 Copyright, todos los derechos reservados.
                 </div>
